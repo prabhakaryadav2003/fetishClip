fd6eb63c447e1f2cc2b12870ec777ea17f92f619
Prabhakar <114614952+prabhakaryadav2003@users.noreply.github.com>
2025-09-21 23:36:43 +0530

    fix: renamed some casing errors

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,5 +1,5 @@
-import { Footer } from "@/components/Footer";
-import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/FooterCard";
+import { Navbar } from "@/components/NavbarCard";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
